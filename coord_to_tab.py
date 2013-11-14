b5257c3e994be9ddba1b19d0e1c46ea32c22cd3c
Marea Cobb <marea.cobb@seattlebiomed.org>
2013-11-14 13:38:54 -0800

    Needs to be rearranged for easier comprehension

--- coord_to_tab.py
+++ coord_to_tab.py
@@ -1,36 +1,37 @@
-__author__ = 'mareac'
+#__author__ = 'mareac'
 
 import re
 import csv
+import sys
+import fileinput
 
 
 
 def readFile():
     header = True
-    f = open('/Users/mareac/Desktop/Scaffolds.LmjF.coords', 'r')
-    new_file = open('/Users/mareac/Desktop/new_file.csv', 'wb')
-    writer = csv.writer(new_file)
-    for line in f:
+    #Adds the header to the input file
+    headerline = "Start of Ref Alignment\t End of Ref Alignment\t Start of Query Alignment\t End of Query Alignment\t"\
+          "Length of Ref Alignment\t Length of Query Alignment\t Percent Identity\t Percent Similarity\t " \
+          "Percent of Stop Codons\t Length of Ref Sequence\t Length of Query Sequence\t Percent Alignment Coverage " \
+          "in Ref Sequence\t Percent Alignment Coverage in Query Sequence\t Ref Reading Frame\t Ref ID\t " \
+          "Query Reading Frame\t Query ID"
+    for lines in fileinput.input():
         if header:
             # Checks for the header line.
-            if line.startswith('='):
-                print 'Switched past header'
+            if lines.startswith('='):
                 header = False
+            elif "S1" in lines:
+                print headerline
             else:
-                header_line = re.split("\[(.*?)]", line)
-                header_line2 = map(str.strip, header_line)
-                header_line3 = filter(None, header_line2)
-                for x in header_line3:
-                    if x == '|':
-                        header_line3.remove(x)
-                writer.writerow(header_line3)
+                print lines
         else:
-            header_line = line.split()
+            header_line = lines.split()
             for x in header_line:
                 if x == '|':
                     header_line.remove(x)
-            writer.writerow(header_line)
-            pass
+            print ('\t'.join(map(str, header_line[0:13]))) + '\t' + header_line[13] + '\t' + header_line[15] + '\t' + \
+                  header_line[14] + '\t' + header_line[16]
+
 
 
 def main():
